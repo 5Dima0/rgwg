ee2bebafa0b50d71366657831dfd4ff71ac453f8
5Dima0 <32190939+5Dima0@users.noreply.github.com>
2017-09-29 09:41:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
+#include <iostream> 
+#include <stdio.h>
+using namespace std;
 
+int main()
+
+{
+	string s, k;
+	int i, p;
+	cin >> s;
+	p = s.length();
+	for (i = p - 1; i >= 0; --i) {
+		k = k + s[i];
+	}
+	if (s == k) {
+		cout << '1';
+	}
+	else {
+		cout << '0';
+	}
+	system("pause");
+
+	return 0;
+}
